7810dc3efc0a5375edc2aae5342144ab5fbe95a1
Harry Thoma <harryjthoma@gmail.com>
2022-01-04 22:37:00 -0800

    Consolidate Types and Variables tests to one file each

--- test/snapshot/Types.smtx.out.cpp
+++ test/snapshot/Types.smtx.out.cpp
@@ -1,7 +1,21 @@
 #include "runtime.h"
 
+struct record_4;
 struct record_0;
+struct record_3;
+struct record_2;
 struct record_1;
+
+struct record_4 : Collectable {
+	record_2* myThing;
+	record_3* stuff;
+};
+void new_record_4() {
+	record_4* obj = (record_4*) gcalloc(sizeof(record_4), 2);
+	obj->stuff = (record_3*) popRoot();
+	obj->myThing = (record_2*) popRoot();
+	pushRoot(obj);
+}
 
 struct record_0 : Collectable {
 	Int* a;
@@ -9,6 +23,32 @@ struct record_0 : Collectable {
 };
 void new_record_0() {
 	record_0* obj = (record_0*) gcalloc(sizeof(record_0), 2);
+	obj->b = (Bool*) popRoot();
+	obj->a = (Int*) popRoot();
+	pushRoot(obj);
+}
+
+struct record_3 : Collectable {
+	Bool* b;
+	String* c;
+	Int* a;
+};
+void new_record_3() {
+	record_3* obj = (record_3*) gcalloc(sizeof(record_3), 3);
+	obj->a = (Int*) popRoot();
+	obj->c = (String*) popRoot();
+	obj->b = (Bool*) popRoot();
+	pushRoot(obj);
+}
+
+struct record_2 : Collectable {
+	Int* a;
+	Bool* b;
+	String* c;
+};
+void new_record_2() {
+	record_2* obj = (record_2*) gcalloc(sizeof(record_2), 3);
+	obj->c = (String*) popRoot();
 	obj->b = (Bool*) popRoot();
 	obj->a = (Int*) popRoot();
 	pushRoot(obj);
@@ -26,14 +66,14 @@ void new_record_1() {
 }
 
 
-Variable* var_y_0;
-Variable* var_x_1;
+Variable* var_x_0;
+Variable* var_y_1;
 void initializeGlobalVariables() {
 	for (int i = 0; i < 2; i++) {
 		new_Variable();
 	}
-	var_y_0 = (Variable*) getRoot(0);
-	var_x_1 = (Variable*) getRoot(1);
+	var_x_0 = (Variable*) getRoot(0);
+	var_y_1 = (Variable*) getRoot(1);
 }
 
 
@@ -41,10 +81,33 @@ int main(int argc, char* argv[]) {
 	initializeGlobalVariables();
 	
 	{
-		pushRoot(var_x_1->val);
-		pushRoot(var_y_0->val);
+		pushRoot(var_x_0->val);
+		pushRoot(var_y_1->val);
 	}
 	new_record_1();
+	popRoot();
+	{
+		new_Int(5);
+		new_Bool(false);
+		new_String("foo");
+	}
+	new_record_2();
+	popRoot();
+	{
+		{
+			new_Int(10);
+			new_Bool(true);
+			new_String("bar");
+		}
+		new_record_2();
+		{
+			new_Bool(true);
+			new_String("baz");
+			new_Int(2);
+		}
+		new_record_3();
+	}
+	new_record_4();
 	popRoot();
 	
 	finalizeGarbageCollector();
--- test/snapshot/Variables.smtx.out.cpp
+++ test/snapshot/Variables.smtx.out.cpp
@@ -1,7 +1,17 @@
 #include "runtime.h"
 
+struct record_2;
 struct record_0;
 struct record_1;
+
+struct record_2 : Collectable {
+	Int* x;
+};
+void new_record_2() {
+	record_2* obj = (record_2*) gcalloc(sizeof(record_2), 1);
+	obj->x = (Int*) popRoot();
+	pushRoot(obj);
+}
 
 struct record_0 : Collectable {
 	Int* n;
@@ -24,10 +34,12 @@ void new_record_1() {
 }
 
 
+Variable* var_x_0;
 void initializeGlobalVariables() {
-	for (int i = 0; i < 0; i++) {
+	for (int i = 0; i < 1; i++) {
 		new_Variable();
 	}
+	var_x_0 = (Variable*) getRoot(0);
 }
 
 struct closure_fun_5_0 : Collectable {
@@ -36,11 +48,11 @@ void fun_5_0() {
 	record_0* arg = (record_0*) getRoot(0);
 	closure_fun_5_0* closure = (closure_fun_5_0*) getRoot(1);
 	new_Variable();
-	Variable* arg_n_0 = (Variable*) getRoot(0);
-	arg_n_0->val = arg->n;
+	Variable* arg_n_1 = (Variable*) getRoot(0);
+	arg_n_1->val = arg->n;
 	
 	{
-		pushRoot(arg_n_0->val);
+		pushRoot(arg_n_1->val);
 	}
 	printint();
 	{
@@ -61,15 +73,15 @@ void fun_11_1() {
 	record_1* arg = (record_1*) getRoot(0);
 	closure_fun_11_1* closure = (closure_fun_11_1*) getRoot(1);
 	new_Variable();
-	Variable* arg_a_1 = (Variable*) getRoot(0);
-	arg_a_1->val = arg->a;
+	Variable* arg_a_2 = (Variable*) getRoot(0);
+	arg_a_2->val = arg->a;
 	new_Variable();
-	Variable* arg_b_2 = (Variable*) getRoot(0);
-	arg_b_2->val = arg->b;
+	Variable* arg_b_3 = (Variable*) getRoot(0);
+	arg_b_3->val = arg->b;
 	
 	{
-		pushRoot(arg_a_1->val);
-		pushRoot(arg_b_2->val);
+		pushRoot(arg_a_2->val);
+		pushRoot(arg_b_3->val);
 	}
 	addint();
 	
@@ -88,36 +100,36 @@ void fun_16_2() {
 	record_0* arg = (record_0*) getRoot(0);
 	closure_fun_16_2* closure = (closure_fun_16_2*) getRoot(1);
 	new_Variable();
-	Variable* arg_n_3 = (Variable*) getRoot(0);
-	arg_n_3->val = arg->n;
+	Variable* arg_n_4 = (Variable*) getRoot(0);
+	arg_n_4->val = arg->n;
 	new_Variable();
-	Variable* var_x_4 = (Variable*) getRoot(0);
+	Variable* var_x_5 = (Variable*) getRoot(0);
 	new_Variable();
-	Variable* var_y_5 = (Variable*) getRoot(0);
+	Variable* var_y_6 = (Variable*) getRoot(0);
 	new_Variable();
-	Variable* var_sum_6 = (Variable*) getRoot(0);
+	Variable* var_sum_7 = (Variable*) getRoot(0);
 	
 	new_Int(1);
-	var_x_4->val = popRoot();
+	var_x_5->val = popRoot();
 	new_Int(2);
-	var_y_5->val = popRoot();
+	var_y_6->val = popRoot();
 	// closure for pattern
 	pushRoot(nullptr);
 	{
-		pushRoot(var_x_4->val);
-		pushRoot(var_y_5->val);
+		pushRoot(var_x_5->val);
+		pushRoot(var_y_6->val);
 	}
 	new_record_1();
 	fun_11_1();
-	var_sum_6->val = popRoot();
+	var_sum_7->val = popRoot();
 	// closure for pattern
 	pushRoot(nullptr);
 	{
 		// closure for pattern
 		pushRoot(nullptr);
 		{
-			pushRoot(var_sum_6->val);
-			pushRoot(arg_n_3->val);
+			pushRoot(var_sum_7->val);
+			pushRoot(arg_n_4->val);
 		}
 		new_record_1();
 		fun_11_1();
@@ -131,6 +143,49 @@ void fun_16_2() {
 void new_fun_16_2() {
 	pushRoot(nullptr);
 	new_Func(fun_16_2);
+}
+struct closure_fun_35_3 : Collectable {
+};
+void fun_35_3() {
+	record_2* arg = (record_2*) getRoot(0);
+	closure_fun_35_3* closure = (closure_fun_35_3*) getRoot(1);
+	new_Variable();
+	Variable* arg_x_8 = (Variable*) getRoot(0);
+	arg_x_8->val = arg->x;
+	
+	{
+		new_Int(6);
+		pushRoot(arg_x_8->val);
+	}
+	addint();
+	
+	ret:
+	Collectable* ret_val = popRoot();
+	popRoots(3);
+	pushRoot(ret_val);
+}
+void new_fun_35_3() {
+	pushRoot(nullptr);
+	new_Func(fun_35_3);
+}
+struct closure_fun_40_4 : Collectable {
+};
+void fun_40_4() {
+	record_0* arg = (record_0*) getRoot(0);
+	closure_fun_40_4* closure = (closure_fun_40_4*) getRoot(1);
+	new_Variable();
+	Variable* arg_n_9 = (Variable*) getRoot(0);
+	arg_n_9->val = arg->n;
+	
+	pushRoot(arg_n_9->val);
+	var_x_0->val = popRoot();
+	
+	ret:
+	popRoots(3);
+}
+void new_fun_40_4() {
+	pushRoot(nullptr);
+	new_Func(fun_40_4);
 }
 
 int main(int argc, char* argv[]) {
@@ -150,6 +205,43 @@ int main(int argc, char* argv[]) {
 	}
 	new_record_0();
 	fun_16_2();
+	new_Int(10);
+	var_x_0->val = popRoot();
+	{
+		// closure for pattern
+		pushRoot(nullptr);
+		{
+			pushRoot(var_x_0->val);
+		}
+		new_record_2();
+		fun_35_3();
+	}
+	printint();
+	{
+		new_String("\n");
+	}
+	printstring();
+	// closure for pattern
+	pushRoot(nullptr);
+	{
+		new_Int(6);
+	}
+	new_record_0();
+	fun_40_4();
+	{
+		// closure for pattern
+		pushRoot(nullptr);
+		{
+			pushRoot(var_x_0->val);
+		}
+		new_record_2();
+		fun_35_3();
+	}
+	printint();
+	{
+		new_String("\n");
+	}
+	printstring();
 	
 	finalizeGarbageCollector();
 	return 0;
